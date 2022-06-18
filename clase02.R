1a2821474b7385f2d1911533676c47e079e023db
mar71n <mar71n@gmail.com>
2022-06-18 20:09:56 -0300

    clase02 group_by, summarise y algunos graficos

--- clase02.R
+++ clase02.R
@@ -1,11 +1,14 @@
 library(dplyr)
 library(readr)
-#install.packages("readr")
+# install.packages("readr")
 library(lubridate)
 
 # https://data.buenosaires.gob.ar/dataset/plan-de-vacunacion-covid-19
-#vacunas <- read_csv("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/plan-de-vacunacion-covid-19/dataset_total_vacunas.csv")
+# vacunas <- read_csv("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/plan-de-vacunacion-covid-19/dataset_total_vacunas.csv")
+# download.file("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/plan-de-vacunacion-covid-19/dataset_total_vacunas.csv", "datos/dataset_total_vacunas.csv")
 vacunas <- read_csv("datos/dataset_total_vacunas.csv")
+
+spec(vacunas)
 
 head(vacunas)
 
@@ -14,10 +17,21 @@ dmy_hms(head(vacunas$FECHA_ADMINISTRACION))
 max(dmy_hms(vacunas$FECHA_ADMINISTRACION))
 
 summary(vacunas)
+# veo que en DOSIS_3 hay 83 NAs
+
+vacunas[which(is.na(vacunas$DOSIS_3)),]
+
+# pongo cero en esos NAs 
+vacunas[which(is.na(vacunas$DOSIS_3)), "DOSIS_3"] <- 0
+
 
 str(vacunas)
 
+names(vacunas)
+
 table(vacunas$GENERO)
+
+vacunas %>% group_by(GENERO) %>% summarise(total = n())
 
 table(vacunas$GRUPO_ETARIO)
 
@@ -32,33 +46,35 @@ vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMIN
 
 library(ggplot2)
 
-
-
 datos <- vacunas %>% mutate(total = as.numeric(DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) 
 
 summary(datos)
 
 table(datos$GENERO)
 
-datos[which(is.na(datos$total)), "total"] <- 0
+vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
+ group_by(GENERO) %>% dplyr::summarise(t=sum(total, na.rm = TRUE))
 
 vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
- group_by(GENERO) %>% dplyr::summarise(t=sum(as.numeric(total)))
-
-
-ggplot(datos, aes(x=, y=total)) + 
-  geom_line()
-
+  group_by(GENERO) %>% dplyr::summarise(t=sum(total))
 
 data1 <- vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
 group_by(GENERO) %>% summarise(t = sum(total, na.rm=TRUE))
 
-ggplot(data1, aes(GENERO,t)) + 
-  geom_bar(stat="identity") 
-
+ggplot(data1, aes(GENERO,t, fill = GENERO)) + 
+  geom_bar(stat="identity", colour = "grey") +
+  geom_text(aes(label= t), vjust= -0.5, color="black", size=4)
 
 data2 <- vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
   group_by(GRUPO_ETARIO, GENERO) %>% summarise(t = sum(total, na.rm=TRUE))
 
 ggplot(data2, aes(GENERO,t, fill=GRUPO_ETARIO)) + 
   geom_bar(stat="identity") 
+
+ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity") 
+
+# grafico de torta:
+# https://es.stackoverflow.com/questions/411130/c%C3%B3mo-hacer-un-gr%C3%A1fico-y-sub-gr%C3%A1ficos-de-pie-en-r
+# https://germangfeler.github.io/datascience/barras-y-tortas/
+ 
