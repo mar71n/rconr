7ea4fcd29045ddaa7f90d7916197d6923d86dad0
mar71n <mar71n@gmail.com>
2022-06-21 09:55:09 -0300

    clase 2 todos los temas

--- clase02.R
+++ clase02.R
@@ -52,8 +52,8 @@ summary(datos)
 
 table(datos$GENERO)
 
-vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
- group_by(GENERO) %>% dplyr::summarise(t=sum(total, na.rm = TRUE))
+#vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
+# group_by(GENERO) %>% dplyr::summarise(t=sum(total, na.rm = TRUE))
 
 vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
   group_by(GENERO) %>% dplyr::summarise(t=sum(total))
@@ -69,7 +69,7 @@ data2 <- vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FE
   group_by(GRUPO_ETARIO, GENERO) %>% summarise(t = sum(total, na.rm=TRUE))
 
 ggplot(data2, aes(GENERO,t, fill=GRUPO_ETARIO)) + 
-  geom_bar(stat="identity") 
+  geom_bar(stat="identity", colour = "grey") 
 
 ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
   geom_bar(stat="identity") 
@@ -78,3 +78,67 @@ ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) +
 # https://es.stackoverflow.com/questions/411130/c%C3%B3mo-hacer-un-gr%C3%A1fico-y-sub-gr%C3%A1ficos-de-pie-en-r
 # https://germangfeler.github.io/datascience/barras-y-tortas/
  
+data2 %>% tally()
+
+data3 <- vacunas %>% mutate(total = (DOSIS_1 + DOSIS_2 + DOSIS_3)) %>% select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, total) %>%
+  group_by(GENERO, GRUPO_ETARIO) %>% summarise(t = sum(total, na.rm=TRUE))
+
+data1 %>% tally()
+data2 %>% tally()
+data3 %>% tally()
+
+ggplot(data3, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity") 
+
+ggplot(data3, aes(GENERO, t, fill=GRUPO_ETARIO)) + 
+  geom_bar(stat="identity") 
+
+
+
+#### Aproximandonos a ggplot
+##### ggplot, aes, stat, position, facet
+# geom_bar por defecto solo requiere aes(x) y hace un count
+dfa <- data.frame(a = c(1,2,3,1), b=c(2,5,2,1))
+ggplot(dfa, aes(a)) + 
+  geom_bar() 
+
+ggplot(dfa, aes(a)) + 
+  geom_bar(stat="count") 
+
+# geom_bar con stat = identity agrupa por x ademas suma todos los casos
+# requiere aes(x, y)
+dfa <- data.frame(a = c(1,2,3), b=c(3,5,2))
+ggplot(dfa, aes(a, b)) + 
+   geom_bar(stat="identity") 
+
+dfa <- data.frame(a = c(1,2,3,1), b=c(2,5,2,1))
+ggplot(dfa, aes(a, b)) + 
+  geom_bar(stat="identity") 
+
+# por defecto position es "stack"
+ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity", position = "stack") 
+
+ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity", position = "dodge", colour = "grey") 
+# el el grafico de arriba, algunos tienen tres columnas otros dos (no hay casos GENERO = N)
+
+# preserve (anchos de columna)
+ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity", position = position_dodge2(preserve = "single"), colour = "grey") 
+
+ggplot(data2, aes(GRUPO_ETARIO, t, fill=GENERO)) + 
+  geom_bar(stat="identity", position = position_dodge2(preserve = "total"), colour = "grey") 
+
+
+# DOSIS_1, DOSIS_2, DOSIS_3, no son tres variables, son tres valore de la variable DOSIS...
+# install.packages("tidyr")
+library(tidyr)
+
+vacunas %>% pivot_longer(cols = c(DOSIS_1, DOSIS_2, DOSIS_3), names_to = "DOSIS", values_to = "aplicadas") %>%
+  select(FECHA_ADMINISTRACION, GENERO, GRUPO_ETARIO, DOSIS, aplicadas) %>%
+  group_by(GRUPO_ETARIO, GENERO, DOSIS) %>% summarise(t = sum(aplicadas)) %>%
+  ggplot(aes(x = GENERO, y = t, fill = GRUPO_ETARIO)) +
+    geom_bar(stat="identity") +
+    facet_wrap(~ DOSIS)
+
