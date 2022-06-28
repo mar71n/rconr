392bff446243363b82b22b9d518a4b8d4f01cb5a
mar71n <mar71n@gmail.com>
2022-06-28 12:53:22 -0300

    clase 3 full_join

--- clase03.R
+++ clase03.R
@@ -33,6 +33,7 @@ n_distinct(poblacion$BARRIO)
 
 c(poblacion %>% distinct(BARRIO))
 
+
 setdiff(establecimientos$BARRIO, poblacion$BARRIO)
 
 setdiff(poblacion$BARRIO, establecimientos$BARRIO)
@@ -40,6 +41,17 @@ setdiff(poblacion$BARRIO, establecimientos$BARRIO)
 which(establecimientos[, "BARRIO"] == "MONTSERRAT")
 which(poblacion[, "BARRIO"] == "MONSERRAT")
 
+establecimientos %>% anti_join(poblacion, by = "BARRIO") %>% filter() %>% select(BARRIO)
+poblacion %>% anti_join(establecimientos, by = "BARRIO") %>% filter()
+
+establecimientos %>% full_join(poblacion, by = "BARRIO") %>% 
+  select(starts_with("NOMBRE"), BARRIO, POBLACION) %>% 
+  filter(is.na(NOMBRE_EST) | is.na(POBLACION) ) %>% 
+  arrange(POBLACION)
+
+full_join(band_members, band_instruments)
+  
+  
 which(establecimientos[, "BARRIO"] == "SAN NIICOLAS")
 which(poblacion[, "BARRIO"] == "SAN NICOLAS")
 
@@ -74,3 +86,26 @@ barrios3 <- establecimientos %>% group_by(BARRIO) %>% summarise(ESTABLECIMIENTOS
 ggplot(barrios2 %>% left_join(barrios3, by = c("barrio" = "BARRIO") )) + 
   geom_sf(aes(fill=CADA_1000))
 
+# left_join(), la mayoria de las veces
+# right_join(), por ejemplo cuando la clave de uno de los lados es compuesta y está en dos tablas
+
+establecimientos %>% count(DE, NIVEL)
+
+establecimientos %>% distinct( NIVEL)
+
+
+
+
+
+
+
+
+
+
+
+# https://data.buenosaires.gob.ar/dataset/plan-de-vacunacion-covid-19
+# vacunas <- read_csv("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/plan-de-vacunacion-covid-19/dataset_total_vacunas.csv")
+# download.file("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/plan-de-vacunacion-covid-19/dataset_total_vacunas.csv", "datos/dataset_total_vacunas.csv")
+vacunas <- read_csv("datos/dataset_total_vacunas.csv")
+
+names(vacunas)
