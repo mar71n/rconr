bcf0487e34fd33d2e0cc62a1d2bd6a11eeebe6b5
mar71n <mar71n@gmail.com>
2022-06-28 09:53:51 -0300

    clase 3 claves unicas

--- clase03.R
+++ clase03.R
@@ -1,44 +1,59 @@
 library(dplyr)
 library(ggplot2)
+library(readr)
 library(sf)
 
 # https://data.buenosaires.gob.ar/dataset/establecimientos-educativos
 # establecimientos <- read_csv("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/establecimientos-educativos/establecimientos_educativos_WGS84.csv")
+# download.file("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/establecimientos-educativos/establecimientos_educativos_WGS84.csv", "datos/establecimientos_educativos_WGS84.csv")
 establecimientos <- read_csv("datos/establecimientos_educativos_WGS84.csv")
 
-str(establecimientos)
+spec(establecimientos)
 
-table(establecimientos$comuna)
+head(establecimientos)
 
-table(establecimientos$barrio)
+names(establecimientos)
+
+names(establecimientos) <- toupper(names(establecimientos))
+
+establecimientos %>% distinct(BARRIO)
+
+c(establecimientos %>% distinct(BARRIO))
+
+n_distinct(establecimientos$BARRIO)
 
 # poblacion <- read_csv("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/barrios/caba_pob_barrios_2010.csv")
 poblacion <- read_csv("datos/caba_pob_barrios_2010.csv")
 
-sum(table(poblacion$BARRIO))
+poblacion %>% count(BARRIO) %>% filter(n != 1)
 
-establecimientos %>% group_by(barrio) %>% summarise(ESTABLECIMIENTOS = n()) %>% mutate(BARRIO = barrio) %>%
-  left_join(poblacion) 
+poblacion %>% distinct(BARRIO)
 
-establecimientos %>% group_by(barrio) %>% summarise(ESTABLECIMIENTOS = n()) %>% mutate(BARRIO = barrio) %>%
-  anti_join(poblacion) 
+n_distinct(poblacion$BARRIO)
 
-which(establecimientos[, "barrio"] == "MONTSERRAT")
+c(poblacion %>% distinct(BARRIO))
+
+setdiff(establecimientos$BARRIO, poblacion$BARRIO)
+
+setdiff(poblacion$BARRIO, establecimientos$BARRIO)
+
+which(establecimientos[, "BARRIO"] == "MONTSERRAT")
 which(poblacion[, "BARRIO"] == "MONSERRAT")
 
-which(establecimientos[, "barrio"] == "SAN NIICOLAS")
+which(establecimientos[, "BARRIO"] == "SAN NIICOLAS")
 which(poblacion[, "BARRIO"] == "SAN NICOLAS")
 
-establecimientos[which(establecimientos[, "barrio"] == "MONTSERRAT"),"barrio"] <- "MONSERRAT"
-establecimientos[which(establecimientos[, "barrio"] == "SAN NIICOLAS"),"barrio"] <- "SAN NICOLAS"
+establecimientos[which(establecimientos[, "BARRIO"] == "MONTSERRAT"),"BARRIO"] <- "MONSERRAT"
+establecimientos[which(establecimientos[, "BARRIO"] == "SAN NIICOLAS"),"BARRIO"] <- "SAN NICOLAS"
+
+establecimientos %>% group_by(BARRIO) %>% 
+  summarise(ESTABLECIMIENTOS = n()) %>% 
+  left_join(poblacion) 
+
 
 establecimientos %>% group_by(barrio) %>% summarise(ESTABLECIMIENTOS = n()) %>%
   left_join(poblacion, by = c("barrio" = "BARRIO") ) 
 
-names(establecimientos)
-names(establecimientos)[22]
-names(establecimientos)[names(establecimientos) == "barrio"]
-names(establecimientos)[names(establecimientos) == "barrio"] <- "BARRIO"
 
 establecimientos %>% group_by(BARRIO) %>% summarise(ESTABLECIMIENTOS = n()) %>%
   left_join(poblacion) 
@@ -50,6 +65,7 @@ establecimientos %>% group_by(BARRIO) %>% summarise(ESTABLECIMIENTOS = n()) %>%
 library(readr)
 
 barrios2 <- st_read("datos/barrios.csv")
+
 barrios3 <- establecimientos %>% group_by(BARRIO) %>% summarise(ESTABLECIMIENTOS = n()) %>%
   left_join(poblacion) %>%
   mutate(CADA_1000 = (ESTABLECIMIENTOS * 1000) / POBLACION)
